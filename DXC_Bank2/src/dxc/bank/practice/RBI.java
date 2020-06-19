e859c3dd935738e2fda5d2ce4c0a5ce318aee872
Vannurswamy <Vannurswamy@LAPTOP-UL60B3NU>
2020-06-19 10:27:28 +0530

    first commit

---
+++ DXC_Bank2/src/dxc/bank/practice/RBI.java
@@ -0,0 +1,11 @@
+package dxc.bank.practice;
+
+import java.io.BufferedReader;
+
+public interface RBI {
+	
+	public void openAccount(BufferedReader buff);
+	public void deposit(BufferedReader buff);
+	public void withdrawl(BufferedReader buff);
+	public void openFD(BufferedReader buff);
+	}
